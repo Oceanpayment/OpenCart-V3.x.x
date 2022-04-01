dd01c2332f9a2647417ad2988ffa1c6f1024c9ef
susan <susanwan@oceanpayment.com.cn>
2022-04-01 11:28:14 +0800

    AR_Cash

---
+++ admin/language/en-gb/extension/payment/op_arcash.php
@@ -0,0 +1,39 @@
+<?php
+// Heading
+$_['heading_title']      	= 'Oceanpayment AR_Cash Payment';
+
+// Text 
+$_['text_payment']       	= 'Payment';
+$_['text_success']       	= 'Success: You have modified AR_Cash account details!';
+$_['text_edit']          	= 'Edit Oceanpayment AR_Cash Payment';
+$_['text_op_arcash'] 	= '<img src="view/image/payment/op_arcash.png" alt="arcash" height="25"  title="arcash" style="border: 1px solid #EEEEEE;" />';
+$_['text_pay']           	= 'https://secure.oceanpayment.com/gateway/service/pay';
+$_['text_test']          	= 'https://secure.oceanpayment.com/gateway/service/test';
+
+
+
+      
+// Entry
+$_['entry_account'] 				= 'Account';
+$_['entry_terminal']   				= 'Terminal';
+$_['entry_securecode']     			= 'Securecode';
+$_['entry_transaction']   			= 'Oceanpayment transaction URL';
+$_['entry_pay_mode']				= 'Pay Mode';
+$_['entry_default_order_status'] 	= 'Default Order Status';
+$_['entry_success_order_status']	= 'Success Order Status';
+$_['entry_failed_order_status'] 	= 'Failed Order Status';
+$_['entry_pending_order_status'] 	= 'Pending Order Status';
+$_['entry_geo_zone']             	= 'Geo Zone';
+$_['entry_status']       			= 'Status';
+$_['entry_sort_order']      		= 'Sort Order';
+
+
+
+
+
+// Error
+$_['error_permission']                = 'Warning: You do not have permission to modify Oceanpayment AR_Cash!';
+$_['error_account']                   = 'Account Required!';
+$_['error_terminal']                  = 'Terminal Required!';
+$_['error_securecode']                = 'Securecode Required!';
+?>
---
+++ catalog/language/en-gb/extension/payment/op_arcash.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']     = 'Thank you for shopping with %s .... ';
+
+// Text
+$_['text_title']        = 'AR_Cash<br/><img src="catalog/view/theme/default/image/op_arcash.png" alt="arcash">';
+$_['text_response']     = 'Response from AR_Cash:';
+$_['text_success']      = 'Your payment was successfully received.';
+$_['text_success_wait'] = 'whilst we finish processing your order.<br>If you are not automatically re-directed in 10 seconds, please click <a href="%s" target="_top" >here</a>.';
+$_['text_failure']      = 'Your payment has been cancelled!';
+$_['text_failure_wait'] = '<br>If you are not automatically re-directed, please click <a href="%s" target="_top" >here</a>.';										  
+
+//Entry
+$_['entry_pay_cpf'] = 'IDN';
+$_['entry_pay_bankCode'] = 'Payment Bank';
+
+?>
