ee7137c89330a466b31e2846b2b19a6cfd65155a
susan <susanwan@oceanpayment.com.cn>
2022-06-09 14:51:38 +0800

    TouchnGo

--- admin/language/en-gb/extension/payment/op_touchngo.php
+++ admin/language/en-gb/extension/payment/op_touchngo.php
@@ -1,12 +1,12 @@
 <?php
 // Heading
-$_['heading_title']      	= 'Oceanpayment Rabbit-LinePay Payment';
+$_['heading_title']      	= 'Oceanpayment TouchnGo Payment';
 
 // Text 
 $_['text_payment']       	= 'Payment';
-$_['text_success']       	= 'Success: You have modified rabbitlinepay account details!';
-$_['text_edit']          	= 'Edit Oceanpayment Rabbit-LinePay Payment';
-$_['text_op_rabbitlinepay'] = '<img src="view/image/payment/op_rabbitlinepay.png" alt="rabbitlinepay" height="25"  title="rabbitlinepay" style="border: 1px solid #EEEEEE;" />';
+$_['text_success']       	= 'Success: You have modified TouchnGo account details!';
+$_['text_edit']          	= 'Edit Oceanpayment TouchnGo Payment';
+$_['text_op_touchngo'] 	    = '<img src="view/image/payment/op_touchngo.png" alt="touchngo" height="25"  title="touchngo" style="border: 1px solid #EEEEEE;" />';
 $_['text_pay']           	= 'https://secure.oceanpayment.com/gateway/service/pay';
 $_['text_test']          	= 'https://test-secure.oceanpayment.com/gateway/service/pay';
 $_['text_pay_iframe']    	= 'Iframe';
@@ -40,7 +40,7 @@ $_['entry_entitys']                 = 'Entity';
 
 
 // Error
-$_['error_permission']                = 'Warning: You do not have permission to modify Oceanpayment Rabbit-LinePay!';
+$_['error_permission']                = 'Warning: You do not have permission to modify Oceanpayment TouchnGo!';
 $_['error_account']                   = 'Account Required!';
 $_['error_terminal']                  = 'Terminal Required!';
 $_['error_securecode']                = 'Securecode Required!';
--- catalog/model/extension/payment/op_touchngo.php
+++ catalog/model/extension/payment/op_touchngo.php
@@ -1,14 +1,14 @@
 <?php 
-class ModelExtensionPaymentOPRabbitlinepay extends Model {
+class ModelExtensionPaymentOPTouchngo extends Model {
 	private $_limit = ',';
 	
   	public function getMethod($address) {
-		$this->load->language('extension/payment/op_rabbitlinepay');
+		$this->load->language('extension/payment/op_touchngo');
 		
-		if ($this->config->get('payment_op_rabbitlinepay_status')) {
-      		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_op_rabbitlinepay_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+		if ($this->config->get('payment_op_touchngo_status')) {
+      		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('payment_op_touchngo_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 			
-			if (!$this->config->get('payment_op_rabbitlinepay_geo_zone_id')) {
+			if (!$this->config->get('payment_op_touchngo_geo_zone_id')) {
         		$status = true;
       		} elseif ($query->num_rows) {
       		  	$status = true;
@@ -23,10 +23,10 @@ class ModelExtensionPaymentOPRabbitlinepay extends Model {
 	
 		if ($status) {  
       		$method_data = array( 
-        		'code'         => 'op_rabbitlinepay',
+        		'code'         => 'op_touchngo',
         		'title'      => $this->language->get('text_title'),
       			'terms'      => '',
-				'sort_order' => $this->config->get('payment_op_rabbitlinepay_sort_order')
+				'sort_order' => $this->config->get('payment_op_touchngo_sort_order')
       		);
     	}
    
