6d420f5dc0e1230f020ef51f0c70923e32710bb6
susan <susanwan@oceanpayment.com.cn>
2022-07-05 18:12:53 +0800

    update setting

--- catalog/language/en-gb/extension/payment/op_korewallet.php
+++ catalog/language/en-gb/extension/payment/op_korewallet.php
@@ -3,7 +3,7 @@
 $_['heading_title']     = 'Thank you for shopping with %s .... ';
 
 // Text
-$_['text_title']        = 'KOR_eWallet Payment <br/><img src="catalog/view/theme/default/image/op_korewallet.png" alt="KOR_eWallet">';
+$_['text_title']        = '간편결제 <br/><img src="catalog/view/theme/default/image/op_korewallet.png" alt="KOR_eWallet">';
 $_['text_response']     = 'Response from KOR_eWallet:';
 $_['text_success']      = 'Your payment was successfully received.';
 $_['text_success_wait'] = 'whilst we finish processing your order.<br>If you are not automatically re-directed in 10 seconds, please click <a href="%s" target="_top" >here</a>.';
