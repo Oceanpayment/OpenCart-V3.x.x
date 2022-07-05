651ac28ae068a09a7cbd6a66597bca02ce942ea4
susan <susanwan@oceanpayment.com.cn>
2022-07-05 18:39:26 +0800

    update setting

--- catalog/language/en-gb/extension/payment/op_korcreditcard.php
+++ catalog/language/en-gb/extension/payment/op_korcreditcard.php
@@ -3,7 +3,7 @@
 $_['heading_title']     = 'Thank you for shopping with %s .... ';
 
 // Text
-$_['text_title']        = 'KOR_Credit Card Payment <br/><img src="catalog/view/theme/default/image/op_korcreditcard.png" alt="KOR_Credit Card">';
+$_['text_title']        = '신용카드 <br/><img src="catalog/view/theme/default/image/op_korcreditcard.png" alt="KOR_Credit Card">';
 $_['text_response']     = 'Response from KOR_Credit Card:';
 $_['text_success']      = 'Your payment was successfully received.';
 $_['text_success_wait'] = 'whilst we finish processing your order.<br>If you are not automatically re-directed in 10 seconds, please click <a href="%s" target="_top" >here</a>.';
