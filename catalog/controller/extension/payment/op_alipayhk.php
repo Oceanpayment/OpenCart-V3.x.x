21bcf38728c7192ce86bf3b395557834752f7ec5
susan <susanwan@oceanpayment.com.cn>
2022-11-11 11:39:29 +0800

    AlipayHK

--- catalog/controller/extension/payment/op_alipayhk.php
+++ catalog/controller/extension/payment/op_alipayhk.php
@@ -371,6 +371,10 @@ class ControllerExtensionPaymentOPAlipayHK extends Controller {
 	
 
 			$message = self::BrowserReturn;
+			if($this->config->get('payment_op_alipayhk_transaction') == 'https://test-secure.oceanpayment.com/gateway/service/pay'){
+				$message .= 'TEST ORDER-';
+				$data['payment_details'] = 'TEST ORDER-'.$data['payment_details'];
+			}
 			if ($payment_status == 1){           //交易状态
 				$message .= 'PAY:Success.';
 			}elseif ($payment_status == 0){
@@ -508,6 +512,9 @@ class ControllerExtensionPaymentOPAlipayHK extends Controller {
 				
 
 				$message = self::PUSH;
+				if($this->config->get('payment_op_alipayhk_transaction') == 'https://test-secure.oceanpayment.com/gateway/service/pay'){
+					$message .= 'TEST ORDER-';
+				}
 				if ($_REQUEST['payment_status'] == 1){           //交易状态
 					$message .= 'PAY:Success.';
 				}elseif ($_REQUEST['payment_status'] == 0){
