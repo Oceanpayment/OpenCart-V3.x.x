3c34f48103bb26c3d8ce79656aa60d559677414d
susan <susanwan@oceanpayment.com.cn>
2022-11-11 17:06:04 +0800

    KOR CreditCard

--- catalog/model/extension/payment/op_korcreditcard.php
+++ catalog/model/extension/payment/op_korcreditcard.php
@@ -21,10 +21,14 @@ class ModelExtensionPaymentOPKorcreditcard extends Model {
 		
 		$method_data = array();
 	
-		if ($status) {  
+		if ($status) {
+			$title = $this->language->get('text_title');
+			if($this->config->get('payment_op_korcreditcard_transaction') == 'https://test-secure.oceanpayment.com/gateway/service/pay'){
+				$title = $this->language->get('text_title').'<br><p style="color: red;">Note: In the test state all transactions are not deducted and cannot be shipped or services provided. The interface needs to be closed in time after the test is completed to avoid consumers from placing orders.</p>';
+			}
       		$method_data = array( 
         		'code'         => 'op_korcreditcard',
-        		'title'      => $this->language->get('text_title'),
+        		'title'      => $title,
       			'terms'      => '',
 				'sort_order' => $this->config->get('payment_op_korcreditcard_sort_order')
       		);
