5d921c8728382c3aa50158670bb9ffe6f29c6fc2
susan <susanwan@oceanpayment.com.cn>
2022-12-22 09:55:25 +0800

    JeniusPay

---
+++ admin/language/en-gb/extension/payment/op_jeniuspay.php
@@ -0,0 +1,48 @@
+<?php
+// Heading
+$_['heading_title']      	= 'Oceanpayment JeniusPay Payment';
+
+// Text 
+$_['text_payment']       	= 'Payment';
+$_['text_success']       	= 'Success: You have modified JeniusPay account details!';
+$_['text_edit']          	= 'Edit Oceanpayment JeniusPay Payment';
+$_['text_op_jeniuspay'] 	        = '<img src="view/image/payment/op_JeniusPay.png" alt="JeniusPay" height="25"  title="JeniusPay" style="border: 1px solid #EEEEEE;" />';
+$_['text_pay']           	= 'https://secure.oceanpayment.com/gateway/service/pay';
+$_['text_test']          	= 'https://test-secure.oceanpayment.com/gateway/service/pay';
+$_['text_pay_iframe']    	= 'Iframe';
+$_['text_pay_redirect']  	= 'Redirect';
+$_['text_select_currency']  = '- Select -';
+$_['text_pay_sandbox']      = 'Note: In the test state all transactions are not deducted and cannot be shipped or services provided. The interface needs to be closed in time after the test is completed to avoid consumers from placing orders.';
+
+      
+// Entry
+$_['entry_account'] 				= 'Account';
+$_['entry_terminal']   				= 'Terminal';
+$_['entry_securecode']     			= 'Securecode';
+$_['entry_transaction']   			= 'Oceanpayment transaction URL';
+$_['entry_pay_mode']				= 'Pay Mode';
+$_['entry_default_order_status'] 	= 'Default Order Status';
+$_['entry_success_order_status']	= 'Success Order Status';
+$_['entry_failed_order_status'] 	= 'Failed Order Status';
+$_['entry_pending_order_status'] 	= 'Pending Order Status';
+$_['entry_geo_zone']             	= 'Geo Zone';
+$_['entry_status']       			= 'Status';
+$_['entry_sort_order']      		= 'Sort Order';
+
+$_['text_hide']                     = 'Hide';
+$_['text_show']                     = 'Show';
+$_['text_hides']                    = 'Hide';
+$_['text_shows']                    = 'Show';
+
+$_['entry_location']                = 'Show/Hide Location';
+$_['entry_locations']               = 'Location';
+$_['entry_entity']                  = 'Show/Hide Entity';
+$_['entry_entitys']                 = 'Entity';
+
+
+// Error
+$_['error_permission']                = 'Warning: You do not have permission to modify Oceanpayment JeniusPay!';
+$_['error_account']                   = 'Account Required!';
+$_['error_terminal']                  = 'Terminal Required!';
+$_['error_securecode']                = 'Securecode Required!';
+?>
